83f9fd6f5a2f6331fe5789b443cee5dbc2cc4586
renan-ranelli <renanranelli@gmail.com>
2014-09-23 01:47:51 -0300

    Whitespace??? Seriously ???

--- src/test/java/inf319/listaMateriais/PartTest.java
+++ src/test/java/inf319/listaMateriais/PartTest.java
@@ -3,7 +3,6 @@ package inf319.listaMateriais;
 import junit.framework.TestCase;
 
 public class PartTest extends TestCase {
-
     public void testPart() {
         PartNumber number = new PartNumber(42);
         Part part = new PiecePart(number, "description", 23);
@@ -12,15 +11,16 @@ public class PartTest extends TestCase {
         part.setDescription("new description");
         assertEquals("new description", part.getDescription());
     }
-    
+
     public void testPiecePart() {
         PartNumber number = new PartNumber(42);
         PiecePart part = new PiecePart(number, "description", 23);
         assertEquals(23, part.cost(), 0);
+
         part.setCost(5);
         assertEquals(5, part.cost(), 0);
     }
-    
+
     public void testAssembly() {
         PartNumber number1 = new PartNumber(23);
         PartNumber number2 = new PartNumber(42);
@@ -39,7 +39,7 @@ public class PartTest extends TestCase {
             assembly2.addPart(new PiecePart(new PartNumber(i), "pp" + i, i));
         }
         assertEquals(((19 + 10) * 10)/2, assembly2.cost(), 0);
-        
+
         assembly3.addPart(assembly1);
         assembly3.addPart(assembly2);
         assertEquals(assembly1.cost() + assembly2.cost(), assembly3.cost(), 0);
@@ -48,42 +48,39 @@ public class PartTest extends TestCase {
         assembly2.addPart(new PiecePart(new PartNumber(75), "piece", 10));
         assertEquals(cost + 10, assembly3.cost(), 0);
     }
-    
+
     public void testList() {
-    	
+
     	// Átomos para a roda dianteira
     	PiecePart aroDianteiro = new PiecePart(new PartNumber(51), "Aro Dianteiro", 20.0);
     	PiecePart cuboDianteiro = new PiecePart(new PartNumber(52), "Cubo Dianteiro", 30.0);
     	PiecePart raiosDianteiro = new PiecePart(new PartNumber(53), "Raios Dianteiro", 5.0);
     	PiecePart pneuDianteiro = new PiecePart(new PartNumber(54), "Pneu Dianteiro", 15.0);
-    	
+
     	// Roda dianteira
     	Assembly rodaDianteira = new Assembly(new PartNumber(5), "Roda Dianteira");
     	rodaDianteira.addPart(aroDianteiro);
     	rodaDianteira.addPart(cuboDianteiro);
     	rodaDianteira.addPart(raiosDianteiro);
     	rodaDianteira.addPart(pneuDianteiro);
-    	
+
     	// Átomos para a roda traseira
        	PiecePart aroTraseiro = new PiecePart(new PartNumber(61), "Aro Traseiro", 20.0);
     	PiecePart cuboTraseiro = new PiecePart(new PartNumber(62), "Cubo Traseiro", 30.0);
     	PiecePart raiosTraseiro = new PiecePart(new PartNumber(63), "Raios Traseiro", 5.0);
     	PiecePart pneuTraseiro = new PiecePart(new PartNumber(64), "Pneu Traseiro", 15.0);
-    	
+
     	//Roda Traseira
     	Assembly rodaTraseira = new Assembly(new PartNumber(6), "Roda Traseira");
     	rodaTraseira.addPart(aroTraseiro);
     	rodaTraseira.addPart(cuboTraseiro);
     	rodaTraseira.addPart(raiosTraseiro);
     	rodaTraseira.addPart(pneuTraseiro);
-    	
+
     	Assembly motocicleta = new Assembly(new PartNumber(7), "Motocicleta");
     	motocicleta.addPart(rodaDianteira);
     	motocicleta.addPart(rodaTraseira);
-    
-    	assertEquals(140.0, motocicleta.cost());
-    	
-    	
-    }
 
+    	assertEquals(140.0, motocicleta.cost());
+    }
 }
